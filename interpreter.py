bbffa45d5169e54d2d1aecf0251001d23e98ed61
iqnite <iqnitetechnologies@gmail.com>
2026-05-18 19:18:26 +0200

    Implement scanner

---
+++ interpreter.py
@@ -0,0 +1,8 @@
+"""
+This module contains the main interpreter for the Spamoji programming language.
+"""
+
+from interpreter.main import main
+
+if __name__ == "__main__":
+    main()
---
+++ interpreter/token.py
@@ -0,0 +1,58 @@
+from enum import Enum
+
+
+class TokenType(Enum):
+    LEFT_PAREN = 0
+    RIGHT_PAREN = 1
+    IDENTIFIER = 3
+    STRING = 4
+    NUMBER = 5
+    COMMENT = 6
+    FUNCTION = 7
+    WHILE = 8
+    BREAK = 9
+    CONTINUE = 10
+    RETURN = 11
+    EQUALS = 12
+    NOT_EQUALS = 13
+    GREATER_THAN = 14
+    LESS_THAN = 15
+    IF = 16
+    IFTRUE = 17
+    ELSE = 18
+    TRUE = 19
+    FALSE = 20
+    VAR = 21
+    LABEL = 22
+    JUMP = 23
+    IMPORT = 33
+    PYTHON = 34
+    OR = 36
+    NOT = 37
+    PLUS = 38
+    MINUS = 39
+    MULTIPLY = 40
+    DIVIDE = 41
+    STOP = 42
+    ERROR = 43
+
+
+class Token:
+    """Represents a token."""
+
+    def __init__(self, token_type: TokenType, lexeme: str, literal: object, line: int):
+        """
+        Initializes a token instance.
+
+        :param TokenType token_type: The type of the token
+        :param str lexeme: The actual text of the token
+        :param object literal: The literal value of the token, if applicable
+        :param int line: The line number where the token was found
+        """
+        self.token_type = token_type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def __str__(self) -> str:
+        return f"{self.token_type} {self.lexeme} {self.literal}"
