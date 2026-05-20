05c975d81c24babc6812b718e6758e81a52ac73e
iqnite <iqnitetechnologies@gmail.com>
2026-05-20 13:22:31 +0200

    Add enclosing environment support

--- interpreter/environment.py
+++ interpreter/environment.py
@@ -8,17 +8,27 @@ from interpreter.token import Token
 
 class Environment:
     values: dict[str, object]
+    enclosing: "Environment | None"
 
-    def __init__(self):
+    def __init__(self, enclosing: "Environment | None" = None):
         self.values = {}
+        self.enclosing = enclosing
 
     def define(self, name: str, value: object):
         self.values[name] = value
 
-    def assign(self, token: Token, value: object):
-        self.values[token.lexeme] = value
+    def assign(self, name: Token, value: object):
+        if name.lexeme in self.values:
+            self.values[name.lexeme] = value
+            return
+        if self.enclosing is not None:
+            self.enclosing.assign(name, value)
+            return
+        raise SpamojiRuntimeError(name, "Undefined variable '" + name.lexeme + "'.")
 
     def get(self, name: Token) -> object:
         if name.lexeme in self.values:
             return self.values[name.lexeme]
+        if self.enclosing is not None:
+            return self.enclosing.get(name)
         raise SpamojiRuntimeError(name, f"Undefined variable '{name.lexeme}'.")
