674920a7afb260892d85e49b4c95771bd0f2add3
iqnite <iqnitetechnologies@gmail.com>
2026-09-16 22:52:05 +0200

    Simplify function decorator syntax

--- spamoji/functions.py
+++ spamoji/functions.py
@@ -65,7 +65,7 @@ class ContinueLoop(RuntimeError):
     pass
 
 
-def spamoji_function(emoji: str | None = None) -> typing.Callable:
+def spamoji_function(emoji_or_func: str | typing.Callable | None = None) -> typing.Callable:
     """
     Decorator to mark a function as a Spamoji native function.
     The function will be registered as a native function in the Spamoji interpreter.
@@ -101,4 +101,8 @@ def spamoji_function(emoji: str | None = None) -> typing.Callable:
         setattr(func, "_spamoji_callable", spamoji_callable)
         return func
 
+    if isinstance(emoji_or_func, type):
+        emoji = None
+        return decorator(emoji_or_func)
+    emoji = emoji_or_func
     return decorator
